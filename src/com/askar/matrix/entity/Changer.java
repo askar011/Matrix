c5821afc73e088f6d3128e32e732d03128d11b5c
Askar <sanbayev99@gmail.com>
2019-06-17 22:32:24 +0600

    fixed with barier and lock

--- src/com/askar/matrix/entity/Changer.java
+++ src/com/askar/matrix/entity/Changer.java
@@ -5,8 +5,8 @@ import com.askar.matrix.singleton.Matrix;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Semaphore;
 
 public class Changer implements Runnable {
 
@@ -14,37 +14,38 @@ public class Changer implements Runnable {
     private DataReader dataReader = new DataReader();
     private String name;
     private Matrix matrix = Matrix.getInstance();
-    private boolean run = true;
     private String filePath;
-    private int c = 0;
-    private Semaphore semaphore;
+    private int changes = 0;
+    private CyclicBarrier cyclicBarrier;
 
-    public Changer(String name, int fileNumber, Semaphore semaphore) {
+    public Changer(String name, int fileNumber, CyclicBarrier cyclicBarrier) {
         this.name = name;
-        this.semaphore = semaphore;
         this.filePath = "data/thread" + fileNumber + ".txt";
+        this.cyclicBarrier = cyclicBarrier;
     }
 
     @Override
     public void run() {
-        while (run) {
-            for (int i = 0; i < matrix.getMatrix().length; i++) {
-                for (int j = 0; j < matrix.getMatrix().length; j++) {
+        for (int i = 0; i < matrix.getMatrix().length; i++) {
+            for (int j = 0; j < matrix.getMatrix().length; j++) {
+                if (changes == 1) {
                     try {
-                        semaphore.acquire();
-                    } catch (InterruptedException e) {
+                        cyclicBarrier.await();
+                    } catch (InterruptedException | BrokenBarrierException e) {
                         LOGGER.error(e);
                     }
-                    if (c < 2 && i == j && (!matrix.getMatrix()[i][j].getChanged())){
+                }
+                matrix.getMatrix()[i][j].getLock().lock();
+                try {
+                    if (i == j && (!matrix.getMatrix()[i][j].getChanged())) {
                         int number = dataReader.readDataFromFile(filePath);
                         matrix.getMatrix()[i][j].setNumber(number);
                         LOGGER.info(name + " thread changed" + " matrix [" + i + "][" + j + "] to:" + number);
                         matrix.getMatrix()[i][j].setChanged(true);
-                        c++;
-                    } else{
-                        run = false;
+                        changes++;
                     }
-                    semaphore.release();
+                } finally {
+                    matrix.getMatrix()[i][j].getLock().unlock();
                 }
             }
         }
--- src/com/askar/matrix/main/Main.java
+++ src/com/askar/matrix/main/Main.java
@@ -3,19 +3,20 @@ package com.askar.matrix.main;
 import com.askar.matrix.entity.Changer;
 import com.askar.matrix.report.Report;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CyclicBarrier;
 
 public class Main {
+    private static final int NUMBER_OF_THREADS = 5;
 
     public static void main(String[] args) throws InterruptedException {
         Report report = new Report();
-        Semaphore semaphore = new Semaphore(1);
+        CyclicBarrier barrier = new CyclicBarrier(NUMBER_OF_THREADS);
 
-        new Thread(new Changer("1", 0, semaphore)).start();
-        new Thread(new Changer("2", 1, semaphore)).start();
-        new Thread(new Changer("3", 2, semaphore)).start();
-        new Thread(new Changer("4", 3, semaphore)).start();
-        new Thread(new Changer("5", 4, semaphore)).start();
+        new Thread(new Changer("1", 0, barrier)).start();
+        new Thread(new Changer("2", 1, barrier)).start();
+        new Thread(new Changer("3", 2, barrier)).start();
+        new Thread(new Changer("4", 3, barrier)).start();
+        new Thread(new Changer("5", 4, barrier)).start();
 
 
         Thread.sleep(1000);
